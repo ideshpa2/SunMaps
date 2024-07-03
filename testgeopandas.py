aa12c303e31823e30195463ebd2ab8f6d360300f
Sofie Vargas <sofievargas06@gmail.com>
2024-07-02 21:34:11 -0700

    a simple interactive map of phoenix

--- testgeopandas.py
+++ testgeopandas.py
@@ -1,4 +1,12 @@
 import geopandas as gpd
-
-gdf = gpd.read_file('/Users/sofievargas/SunMaps/Zoning.shp')
-gdf.explore("area", legend = True)
+import matplotlib.pyplot as plt
+import folium
+import geodatasets
+import webbrowser
+phoenix = geodatasets.get_path("geoda.phoenix_acs ")
+print(phoenix)
+gdf = gpd.read_file(phoenix)
+map = gdf.plot()
+interactivemap = gdf.explore()
+plt.show()
+interactivemap.save("phoenix.html")
