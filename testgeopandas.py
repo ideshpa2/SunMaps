7525ae83fd2122fda95d0ea4138d0f31d2a67036
ideshpa2 <143305807+ideshpa2@users.noreply.github.com>
2024-07-03 05:15:14 -0700

    Add files via upload
    adding marker

--- testgeopandas.py
+++ testgeopandas.py
@@ -1,14 +1,32 @@
+# importing
 import geopandas as gpd
 import matplotlib.pyplot as plt
 import folium
 import geodatasets
 import webbrowser
-phoenix = geodatasets.get_path("geoda.phoenix_acs ")
-gdfzoning = gpd.read_file("Zoning.shp")
+
+# loading data
+phoenix = geodatasets.get_path("geoda.phoenix_acs")
 gdf = gpd.read_file(phoenix)
-map = gdf.plot()
-zoningmap = gdfzoning.explore()
-zoningmap.save("zoning.html")
-interactivemap = gdf.explore()
-plt.show()
-interactivemap.save("phoenix.html")
+gdfzoning = gpd.read_file("Zoning.shp")
+
+# centering map
+center = [gdf.geometry.centroid.y.mean(), gdf.geometry.centroid.x.mean()]
+phoenix_map = folium.Map(location=center, zoom_start=10)
+
+folium.GeoJson(gdf).add_to(phoenix_map)
+
+# coords for most walkable phx area
+marker_location = [33.4484, -112.0740]
+
+# marking location
+folium.Marker(location=marker_location,
+              popup='testing').add_to(phoenix_map)
+
+# saving map
+phoenix_map.save("phoenix_interactive.html")
+ax = gdfzoning.plot()
+plt.savefig("zoning.png")
+
+# opening
+webbrowser.open("phoenix_interactive.html")
