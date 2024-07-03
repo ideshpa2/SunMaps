c87e486a11db24b33e98a03044ea78be5e554988
Sofie Vargas <sofievargas06@gmail.com>
2024-07-02 21:39:39 -0700

    tested geopandas with uploaded zoning shapefile...

--- testgeopandas.py
+++ testgeopandas.py
@@ -4,9 +4,11 @@ import folium
 import geodatasets
 import webbrowser
 phoenix = geodatasets.get_path("geoda.phoenix_acs ")
-print(phoenix)
+gdfzoning = gpd.read_file("Zoning.shp")
 gdf = gpd.read_file(phoenix)
 map = gdf.plot()
+zoningmap = gdfzoning.explore()
+zoningmap.save("zoning.html")
 interactivemap = gdf.explore()
 plt.show()
 interactivemap.save("phoenix.html")
