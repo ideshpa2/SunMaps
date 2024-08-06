c2098137c5faf5636f16f76bd063d454403d11a8
Sofie Vargas <sofievargas06@gmail.com>
2024-08-05 20:40:45 -0700

    added decision table stuff

--- distributions.py
+++ distributions.py
@@ -3,6 +3,111 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
+#reflective pavement infrastructure
+def pavement_infrastructure(lcz, mrt, zoning):
+    lowrise_lczs = (
+        
+            "LCZ 9 (Sparsely Built)", 
+            "LCZ D (Low Plants)",   
+            "LCZ E (Bare rock or paved)"
+    )
+    highrise_lczs = (
+            "LCZ 5 (Open Midrise)", 
+            "LCZ 6 (Open Low Rise)",
+            "LCZ 8 (Large Low-rise)",
+            "LCZ 10 (Heavy Industry)"
+    )
+    if mrt > medians[lcz]:
+        albedo = 0.7
+    if mrt == medians[lcz]:
+        albedo = 0.5
+    if mrt < medians[lcz]:
+        albedo = 0.3
+
+    if lcz in lowrise_lczs:
+        projected_mrt = mrt-27
+    
+    if lcz in  highrise_lczs:
+        projected_mrt = mrt-14
+    
+    return f"For LCZ '{lcz}', MRT {mrt}, and zoning '{zoning}',
+            we recommend that reflective pavement of a {albedo} albedo 
+            would result in a projected mean radiant temperature  of {projected_mrt} °C"
+
+
+
+#cool roof infrastructure
+def cool_roof_infrastructure(lcz, mrt, zoning):
+    best_lczs = (
+            "LCZ 4 (Open High-rise)",
+            "LCZ 5 (Open Midrise)", 
+            "LCZ 7 (Lightweight Low-rise)", 
+            "LCZ 8 (Large Low-rise)",
+            "LCZ 10 (Heavy Industry)",
+            "LCZ B (Scattered Trees)",
+            "LCZ D (Low Plants)" 
+    )
+    if lcz in best_lczs and zoning == "residential":
+        projected_mrt = mrt-0.3
+    
+    return f"For LCZ '{lcz}', MRT {mrt}, and zoning '{zoning}',
+            recommended cool roofs would result in a projected mean radiant temperature  of {mrt - 8.2} °C"
+
+#water_mister_infrastructure
+def water_mister_infrastructure(lcz, mrt, zoning):
+    best_lczs = (
+            "LCZ 4 (Open High-rise)",
+            "LCZ 5 (Open Midrise)", 
+            "LCZ 7 (Lightweight Low-rise)", 
+            "LCZ 8 (Large Low-rise)",
+            "LCZ 10 (Heavy Industry)"
+    )
+    if(lcz in best_lczs):
+        if(zoning == 'commercial' or zoning == 'industrial'):
+            solution = f"For LCZ '{lcz}', MRT {mrt}, and zoning '{zoning}',
+            recommended water misters would result in a projected mean radiant temperature  of {mrt - 8.2} °C"
+    return solution
+        
+#Tree Canopy "Table"
+        
+def tree_canopy_infrastructure(lcz, mrt, zoning):
+    #trees work best in low rise, low density neighborhoods
+    projected_mrt = mrt
+    best_lczs = (
+        "LCZ 6 (Open Low Rise)",
+        "LCZ 7 (Lightweight Low-rise)", 
+        "LCZ 8 (Large Low-rise)",
+        "LCZ 9 (Sparsely Built)" 
+        )
+
+    
+    if lcz in best_lczs:
+        if mrt > medians[lcz]:
+            tree_type = "Non-native deciduous and evergreen trees"
+            projected_mrt-=13
+        elif mrt == medians[lcz]:
+            tree_type = "Native trees"
+            projected_mrt -= 8.8
+        else:
+            tree_type = "Phoenix Palm trees"
+            projected_mrt -= 5.6
+
+    percentage = 0
+    tree_type = ""
+    if(zoning == "commercial"):
+        percentage = 15
+    if(zoning == "industrial"):
+        percentage = 10
+    if(zoning == "residential"):
+        percentage = 25
+    
+    solution += f"For LCZ '{lcz}', MRT {mrt}, and zoning '{zoning}',
+            we recommend a {percentage}% increase in tree canopy cover with {tree_type},
+            with a projeted mean radiant temperature of {projected_mrt} °C."
+    return solution
+
+
+
 # Load your data
 with open('lcz_mrt_data.json', 'r') as f:
     lcz_mrt_data = json.load(f)
@@ -10,13 +115,20 @@ with open('lcz_mrt_data.json', 'r') as f:
 
 
 # Create plots for each LCZ
+medians = {}
 for lcz, mrts in lcz_mrt_data.items():
     mrts_non_zero = [mrt for mrt in mrts if mrt != 0]
     mrt_mean = np.mean(mrts_non_zero)
     mrt_median = np.median(mrts_non_zero)
     mrt_range = (min(mrts_non_zero), max(mrts_non_zero))
+    medians[lcz] = mrt_median
 
 
+
+
+
+
+"""
     print(lcz)
     plt.figure(figsize=(10, 6))
     sns.histplot(mrts_non_zero, kde=True)  # kde=True to add the kernel density estimate (bell curve)
@@ -36,7 +148,7 @@ for lcz, mrts in lcz_mrt_data.items():
     plt.grid(True)
     plt.savefig(f"mrt_distributions/{lcz}_distribution.png")
 
-"""
+
 /Users/sofievargas/SunMaps-2/mrt_distributions
 import json
 import requests
@@ -127,102 +239,4 @@ for i in range(height):
 
 lcz_mrt_data_str = "data:text/json;charset=utf-8," + json.dumps(lcz_mrt_map)
 with open("lcz_mrt_data.json", "w") as f:
-    f.write(json.dumps(lcz_mrt_map))
-
-
-
-
-
-
-import json
-from collections import defaultdict
-from PIL import Image
-lcz_mrt_data = defaultdict(list)
-import matplotlib.pyplot as plt
-import os
-
-lcz_image = Image.open("2016_0504_LCZ_PHOENIX_filter_3x3.png")
-lcz_pixels = lcz_image.load()
-
-def map_to_image_coords(lat, lon, bounds, imageSize) {
-        latMin = bounds[0][0], lonMin = bounds[0][1], latMax = bounds[1][0], lonMax = bounds[1][1]
-        imgWidth = imageSize[0], imgHeight = imageSize[1]
-        x = Math.round((lon - lonMin) / (lonMax - lonMin) * imgWidth)
-        y = Math.round((latMax - lat) / (latMax - latMin) * imgHeight)
-        return x, y
-    }
-
-with open('simplified_mrt_data.json', 'r') as file:
-    mrtjson = json.load(file)
-
-transform = mrtjson['transform']
-mrt_data = mrtjson['data']
-
-def pixel_to_geo(transform, px, py):
-    a, b, c, d, e, f = transform[0], transform[1], transform[2], transform[3], transform[4], transform[5]
-    lon = a * px + b * py + c
-    lat = d * px + e * py + f
-    return lon, lat
-
-print(pixel_to_geo(transform, 300, 300))
-
-LCZ = {
-        (140,0,0): "LCZ 4 (Open High-rise)",
-        (209,0,0): "LCZ 5 (Open Midrise)",    
-        (255,0,0): "LCZ 6 (Open Low Rise)" ,
-        (0,106,0): "LCZ A (Dense Trees)",
-        (0,170,0): "LCZ B (Scattered Trees)",
-        (100,133,37): "LCZ C (Bush, Scrub)",
-        (185,219,121): "LCZ D (Low Plants)",
-        (0,0,0): "LCZ E (Bare rock or paved)", 
-        (251,247,174): "LCZ F (Bare Soil or Sand)", 
-        (106,106,255): "LCZ G (Water)", 
-        (191,77,0): "LCZ 4 (Open High-rise)",            
-        (255,102,0): "LCZ 5 (Open Midrise)", 
-        (255,153,85): "LCZ 6 (Open Low Rise)",
-        (250,238,): "LCZ 7 (Lightweight Low-rise)", 
-        (188,188,188): "LCZ 8 (Large Low-rise)",
-        (255,204,170): "LCZ 9 (Sparsely Built)", 
-        (85,85,85): "LCZ 10 (Heavy Industry)"
-}
-
-height = len(mrt_data)
-width = len(mrt_data[0])
-lcz_mrt_data = defaultdict(list)
-print(height, width)
-
-for x in range(width):
-    for y in range(height):
-        val = mrt_data[y][x]
-        if val != 0:
-            lon, lat = pixel_to_geo(transform, x, y)
-            lcz_x, lcz_y = int((lon-transform[2]) / transform[0]), int((lat - transform[5]) / transform[4])
-        # Get the color of the current pixel
-            color = lcz_pixels[x, y]
-            if 0 <= lcz_x < lcz_image.width and 0 <= lcz_y < lcz_image.height:
-                color = lcz_pixels[lcz_x, lcz_y]
-                lcz_val = LCZ.get(color)
-                if lcz_val:
-                    lcz_mrt_data[lcz_val].append(val)
-
-print("mrt data iterated")
-print(len(lcz_mrt_data))
-output_dir = 'mrt_distributions'
-os.makedirs(output_dir, exist_ok=True)
-
-# Create a histogram for each LCZ
-counter = 0
-for lcz, mrt_values in lcz_mrt_data.items():
-    counter += 1
-    plt.figure()
-    plt.hist(mrt_values, bins=30, alpha=0.75, color='blue', edgecolor='black')
-    plt.title(f'MRT Distribution for {lcz}')
-    plt.xlabel('MRT')
-    plt.ylabel('Frequency')
-    plt.grid(True)
-    file_path = os.path.join(output_dir, f"{counter}distribution.png")
-    print(file_path)
-    plt.savefig(f"{counter}distribution.png")
-    plt.show()
-
-print("distributions saved")"""
+    f.write(json.dumps(lcz_mrt_map))"""
